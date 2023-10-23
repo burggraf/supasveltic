e5542efaceef2b91664b089e9be6acce9c49dd37
Mark Burggraf <markb@mantisbible.com>
2023-10-23 06:55:59 -0400

    reconfigure icons

--- vite.config.ts
+++ vite.config.ts
@@ -15,11 +15,11 @@ const pkg = JSON.parse(json);
 
 const config: UserConfig = {
 	// WARN: this will not be necessary on your project
-	logLevel: 'info',
+	// logLevel: 'info',
 	// WARN: this will not be necessary on your project
-	build: {
-		minify: false,
-	},
+	// build: {
+	// 	minify: false,
+	// },
 	// WARN: this will not be necessary on your project
 	define: {
 		__DATE__: `'${new Date().toISOString()}'`,
@@ -65,21 +65,56 @@ const config: UserConfig = {
 					background_color: "#ffffff",
 					icons: [
 						{
-							src: '/pwa-192x192.png',
-							sizes: '192x192',
-							type: 'image/png',
-						},
-						{
-							src: '/pwa-512x512.png',
-							sizes: '512x512',
-							type: 'image/png',
-						},
-						{
-							src: '/pwa-512x512.png',
-							sizes: '512x512',
-							type: 'image/png',
-							purpose: 'any maskable',
-						},
+							"src": "/icon-32.png",
+							"sizes": "32x32",
+							"type": "image/png"
+						  },
+						  {
+							"src": "/icon-64.png",
+							"sizes": "64x64",
+							"type": "image/png"
+						  },
+						  {
+							"src": "/icon-96.png",
+							"sizes": "96x96",
+							"type": "image/png"
+						  },
+						  {
+							"src": "/icon-128.png",
+							"sizes": "128x128",
+							"type": "image/png"
+						  },
+						  {
+							"src": "/icon-152.png",
+							"sizes": "152x152",
+							"type": "image/png"
+						  },
+						  {
+							"src": "/icon-168.png",
+							"sizes": "168x168",
+							"type": "image/png"
+						  },
+						  {
+							"src": "/icon-192.png",
+							"sizes": "192x192",
+							"type": "image/png"
+						  },
+						  {
+							"src": "/icon-256.png",
+							"sizes": "256x256",
+							"type": "image/png"
+						  },
+						  {
+							"src": "/icon-384.png",
+							"sizes": "384x384",
+							"type": "image/png"
+						  },
+						  {
+							"src": "/icon-512.png",
+							"sizes": "512x512",
+							"type": "image/png",
+							"purpose": "any maskable"
+						  },
 					],
 				},
 				injectManifest: {

a96bbcb246166ce4c8d66e635ad890fc84f5038f
Mark Burggraf <markb@mantisbible.com>
2023-10-27 09:59:20 -0400

    added openModal utility function

--- src/services/utils.service.ts
+++ src/services/utils.service.ts
@@ -1,5 +1,6 @@
 // import { goto } from '$app/navigation';
 // import { showConfirm } from '$services/alert.service';
+import { modalController } from '$ionic/svelte'
 
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -12,3 +13,20 @@ export const gen_random_uuid = () => {
 }
 export const uuid_generate_v4 = gen_random_uuid;
 
+export const openModal = async (theModal: any, theProps: any = {}, theOptions: any = {}) => {
+  const obj: any = {
+    component: theModal,
+    componentProps: theProps,
+    showBackdrop: true,
+    backdropDismiss: true,
+  };
+  for (const key in theOptions) {
+    obj[key] = theOptions[key];
+  }
+  console.log('obj', obj)
+  const openLoginModalController = await modalController.create(obj)
+
+  openLoginModalController.present();
+  const { data } = await openLoginModalController.onWillDismiss();
+  return data;
+}
